731dacc9829d6b8c1ce2a44e4dbf9d3c836b3202
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2023-02-07 17:40:28 -0300

    feat: add user repositorie

---
+++ src/repositories/userRepositorie.ts
@@ -0,0 +1,15 @@
+import client from "../database/postgres.js";
+
+async function insertNewUser(name: string, CPF: string, birthday: string) {
+  await client.users.create({ data: { name, CPF, birthday } });
+}
+
+async function findUserByCPF(CPF: string) {
+  return await client.users.findFirst({ where: { CPF } });
+}
+
+async function findUserPage(page: number) {
+  await client.users.findMany();
+}
+
+export { insertNewUser, findUserByCPF, findUserPage };
